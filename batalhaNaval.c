a456a4a35c94ae81eea8578d608648394ef8e3de
Leonardo <198978652+leonardosegfault@users.noreply.github.com>
2025-03-24 22:44:51 +0000

    Nível Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
@@ -31,16 +32,16 @@ int main() {
         tabuleiro[0 + i][9 - i] = 3;
     }
 
-    // Começa a leitura por linha
+    // Percorre as linhas
     for (int y = 0; y < 10; y++) {
         // Percorre as colunas
         for (int x = 0; x < 10; x++) {
             printf("%d ", tabuleiro[x][y]);
         }
 
-        // Finaliza a linha
         printf("\n");
     }
+    printf("\n");
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
@@ -52,16 +53,157 @@ int main() {
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 1 1 1 1 1
+    int cone[5][3] = {0};
+    // Posição horizontal inicial do poder
+    int x_inicial = 2,
+        y_inicial = 0;
+
+    printf("CONE\n");
+    // Percorre todas as linhas
+    for (int y = 0; y < 3; y++) {
+        // Define o tamanho do poder, que nesse caso é igual ao índice da linha atual,
+        // assim permitindo que ele se expanda baseado na altura e formando uma pirâmide.
+        int tamanho = y + 1;
+        for (int i = 0; i < tamanho; i++) {
+            cone[x_inicial + i][y_inicial + y] = 1;
+            cone[x_inicial - i][y_inicial + y] = 1;
+        }
+
+        for (int x = 0; x < 5; x++) {
+            printf("%d ", cone[x][y]);
+        }
+
+        printf("\n");
+    }
+
+    printf("CONE NO TABULEIRO\n");
+    int ctabuleiro[10][10] = {0};
+    // Copia tabuleiro para que possa ser reutilizado sem que os poderes o afete.
+    memcpy(ctabuleiro, tabuleiro, sizeof(tabuleiro));
+
+    int x_atq = 1,
+        y_atq = 6;
+
+    for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < 5; x++) {
+            int valor = cone[x][y];
+
+            // Essa condição previne que os arredores sejam deletados pelo vazio do cone.
+            if (valor != 0) {
+                ctabuleiro[x_atq + x][y_atq + y] = valor;
+            }
+        }
+    }
+
+    // Percorre as linhas
+    for (int y = 0; y < 10; y++) {
+        // Percorre as colunas
+        for (int x = 0; x < 10; x++) {
+            printf("%d ", ctabuleiro[x][y]);
+        }
+
+        printf("\n");
+    }
+    printf("\n");
     
     // Exemplo para habilidade em octaedro:
     // 0 0 1 0 0
     // 0 1 1 1 0
     // 0 0 1 0 0
+    int octaedro[5][3] = {0};
+    int tamanho = 3;
+    x_inicial = 2;
+    y_inicial = 1;
+    
+    printf("OCTAEDRO\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < tamanho; j++) {
+            if (i == 0) {
+                octaedro[2][j] = 1;
+            } else {
+                octaedro[1 + j][1] = 1;
+            }
+        }
+    }
+
+    // Percorre as linhas
+    for (int y = 0; y < 3; y++) {
+        // Percorre as colunas
+        for (int x = 0; x < 5; x++) {
+            printf("%d ", octaedro[x][y]);
+        }
+
+        printf("\n");
+    }
+
+    memcpy(ctabuleiro, tabuleiro, sizeof(tabuleiro));
+    for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < 5; x++) {
+            int valor = octaedro[x][y];
+            if (valor != 0) {
+                ctabuleiro[x_atq + x][y_atq + y] = valor;
+            }
+        }
+    }
+
+    printf("OCTAEDRO NO TABULEIRO\n");
+    // Percorre as linhas
+    for (int y = 0; y < 10; y++) {
+        // Percorre as colunas
+        for (int x = 0; x < 10; x++) {
+            printf("%d ", ctabuleiro[x][y]);
+        }
+
+        printf("\n");
+    }
+    printf("\n");
 
     // Exemplo para habilidade em cruz:
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+    int cruz[5][3] = {0};
+    
+    printf("CRUZ\n");
+    for (int y = 0; y < 3; y++) {
+        cruz[2][y] = 1;
+    }
+    for (int x = 0; x < 5; x++) {
+        cruz[x][1] = 1;
+    }
+
+    // Percorre as linhas
+    for (int y = 0; y < 3; y++) {
+        // Percorre as colunas
+        for (int x = 0; x < 5; x++) {
+            printf("%d ", cruz[x][y]);
+        }
+
+        printf("\n");
+    }
+
+    memset(ctabuleiro, 0, sizeof(ctabuleiro));
+    memcpy(ctabuleiro, tabuleiro, sizeof(tabuleiro));
+    for (int y = 0; y < 3; y++) {
+        for (int x = 0; x < 5; x++) {
+            int valor = cruz[x][y];
+            if (valor != 0) {
+                ctabuleiro[x_atq + x][y_atq + y] = valor;
+            }
+        }
+    }
+
+    printf("CRUZ NO TABULEIRO\n");
+    // Percorre as linhas
+    for (int y = 0; y < 10; y++) {
+        // Percorre as colunas
+        for (int x = 0; x < 10; x++) {
+            printf("%d ", ctabuleiro[x][y]);
+        }
+
+        printf("\n");
+    }
+    printf("\n");
 
     return 0;
 }
